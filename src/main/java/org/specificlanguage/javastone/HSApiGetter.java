1930d96ca266cc7fbd33d69f9ed61521ebc8b725
specificlanguage <specificlanguage@gmail.com>
2020-07-27 19:57:44 -0400

    some preliminary code to get one-time card info

--- src/main/java/org/specificlanguage/javastone/HSApiGetter.java
+++ src/main/java/org/specificlanguage/javastone/HSApiGetter.java
@@ -1,9 +1,14 @@
 package org.specificlanguage.javastone;
 
+import jdk.nashorn.internal.parser.JSONParser;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class HSApiGetter {
@@ -11,13 +16,28 @@ public class HSApiGetter {
     public static void main(String[] args){
         try {
             File file = new File(HSApiGetter.class.getResource("api_key.txt").getFile());
+            InputStream is = new FileInputStream(HSApiGetter.class.getResource("sets.json").getFile());
+
+            // API request
+
             Scanner scanner = new Scanner(file);
             String api_key = scanner.next();
+            scanner.close();
 
             HttpResponse<String> response = Unirest.get("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards")
                     .header("x-rapidapi-host", "omgvamp-hearthstone-v1.p.rapidapi.com")
                     .header("x-rapidapi-key", api_key)
                     .asString();
+
+            String string = response.getBody();
+            JSONObject all_cards = new JSONObject(string);
+            JSONObject all_sets = new JSONObject();
+
+            Iterator<String> keys = all_cards.keys();
+
+
+
+
         } catch(Exception e){
             e.printStackTrace();
         }

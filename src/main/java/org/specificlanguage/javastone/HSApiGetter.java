cd2752219f57ecf9f45d3d1f0249db8cfb20ef60
specificlanguage <specificlanguage@gmail.com>
2020-07-26 22:51:54 -0400

    Made mistakes
    - TODO next:
    - review SpellCard code
    - privated fields
    - redo changes to listeners

--- src/main/java/org/specificlanguage/javastone/HSApiGetter.java
+++ src/main/java/org/specificlanguage/javastone/HSApiGetter.java
@@ -3,12 +3,27 @@ package org.specificlanguage.javastone;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class HSApiGetter {
 
-    HttpResponse<String> response = Unirest.get("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards")
-            .header("x-rapidapi-host", "omgvamp-hearthstone-v1.p.rapidapi.com")
-            .header("x-rapidapi-key", "a1ffbf4cb3mshe3ddc1c5abb2a7bp162759jsna19b890d239c")
-            .asString();
+    public static void main(String[] args){
+        try {
+            File file = new File("api_key.txt");
+            Scanner scanner = new Scanner(file);
+            String api_key = scanner.next();
+
+            HttpResponse<String> response = Unirest.get("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards")
+                    .header("x-rapidapi-host", "omgvamp-hearthstone-v1.p.rapidapi.com")
+                    .header("x-rapidapi-key", api_key)
+                    .asString();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
 
     
 

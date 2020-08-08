ab6ba924c78ac573db3cba85d1dcb7b799f1567a
specificlanguage <specificlanguage@gmail.com>
2020-08-08 09:13:22 -0400

    Revert "Beginnings of a new Action system (still in limbo)"
    This reverts commit 2a589ee92d2e05dfa58b17d0f326cfd8c22f00ac.

--- src/main/java/org/specificlanguage/javastone/entity/attributes/Attribute.java
+++ src/main/java/org/specificlanguage/javastone/entity/attributes/Attribute.java
@@ -7,14 +7,4 @@ public enum Attribute {
     TAUNT, CHARGE, RUSH, STEALTH, SPELL_DAMAGE, DEATHRATTLE, AURA, LIFESTEAL, POISONOUS, WINDFURY, CANT_ATTACK,
     CANT_ATTACK_HEROES;
 
-    public static Attribute matchValue(String s){
-        for(Attribute attribute : Attribute.values()){
-            if(Attribute.valueOf(s) == attribute){
-                return attribute;
-            }
-        }
-
-        throw new IllegalArgumentException();
-    }
-
 }
